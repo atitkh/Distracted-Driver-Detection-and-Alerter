ea677796ae4ab654eb660bb2d868686807037300
Atit Kharel <atit286@gmail.com>
2022-12-19 21:49:06 +0545

    update

--- send_sms.py
+++ send_sms.py
@@ -4,15 +4,9 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 
-<<<<<<< HEAD
 account_sid = ''
 auth_token = ''
 client = Client(st.secrets["account_sid"], st.secrets["auth_token"])
-=======
-account_sid = ''
-auth_token = ''
-client = Client(account_sid, auth_token)
->>>>>>> 321c70a (update)
 
 def sendSMS(messages,phone_number):
     message = client.messages.create(
@@ -20,8 +14,4 @@ def sendSMS(messages,phone_number):
         from_='+12724358071',
         to='+977'+phone_number
     )
-<<<<<<< HEAD
     return
-=======
-    return
->>>>>>> 321c70a (update)
