6dfda680bf01fe2fd5c42fed1a6c6813f108c448
Atit Kharel <atit286@gmail.com>
2022-12-21 23:59:41 +0545

    added streamlit

--- send_sms.py
+++ send_sms.py
@@ -1,11 +1,11 @@
 import os 
 from twilio.rest import Client
+import streamlit as st
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 
-account_sid = ''
-auth_token = ''
+
 client = Client(st.secrets["account_sid"], st.secrets["auth_token"])
 
 def sendSMS(messages,phone_number):
